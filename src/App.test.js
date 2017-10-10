a25c5037207de41132ae538a65036fb1e0ebb97c
Erik Taheri <etaheri@me.com>
2017-10-10 11:30:10 -0400

    add snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestRenderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
 describe('App Component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div'); // eslint-disable-line no-undef
     ReactDOM.render(<App />, div);
+  });
+
+  it('it renders a title', () => {
+    const renderedComponent = shallow(<App />);
+    expect(renderedComponent.find('h2').length).toBe(1);
+  });
+
+  it('renders a snapshot', () => {
+    const tree = ReactTestRenderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
